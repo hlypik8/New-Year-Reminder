8e09dc9bee40169416f9e2d9cd72071ac0e285b8
hlypik <arsenyborovskoi@yandex.ru>
2025-02-13 18:49:35 +0300

    Исходный код задания по теме Дженерики и наследование

---
+++ src/Contact.java
@@ -0,0 +1,10 @@
+// Дополните объявление класса Contact
+public abstract class Contact {
+    // Класс должен содержать одно полe - имя пользователя name
+    private String name;
+
+    // И два метода - sendMessage() для отправки сообщения и print() для печати информации о контакте
+    public void sendMessage();
+
+    public void print();
+}
